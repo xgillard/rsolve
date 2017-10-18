e04f4d8c61067b20e3725f6b25e17277a432c255
Xavier Gillard <xavier.gillard@uclouvain.be>
2017-10-19 00:56:16 +0200

    tests pour les variables and literals

--- src/core.rs
+++ src/core.rs
@@ -91,3 +91,144 @@ impl Not for Literal {
         Literal(-self.0)
     }
 }
+
+#[cfg(test)]
+mod test_variable {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn from_should_fail_on_zero() {
+        assert_eq!("Variable(0)", format!("{:?}", Variable::from(0)));
+    }
+
+    // negatives are caught by the compiler
+
+    #[test]
+    fn from_should_work_on_positive_number() {
+        assert_eq!("Variable(42)", format!("{:?}", Variable::from(42)));
+    }
+
+    #[test]
+    fn to_usize_should_return_raw_value() {
+        assert_eq!(42, Variable::from(42).to_usize());
+    }
+
+    #[test]
+    fn test_equals_is_true_for_same_values() {
+        assert_eq!(Variable(42), Variable::from(42));
+    }
+    #[test]
+    fn test_equals_is_false_for_different_values() {
+        assert_ne!(Variable::from(5), Variable::from(42));
+    }
+}
+
+#[cfg(test)]
+mod test_literal {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn constructor_fails_for_zero(){
+        Literal::from(0);
+    }
+
+    #[test]
+    fn constructor_work_for_positive(){
+        assert_eq!("Literal(1)", format!("{:?}", Literal::from(1)));
+    }
+    #[test]
+    fn constructor_work_for_negative(){
+        assert_eq!("Literal(-1)", format!("{:?}", Literal::from(-1)));
+    }
+    #[test]
+    fn constructor_work_for_var(){
+        assert_eq!("Literal(1)", format!("{:?}", Literal::from_var(Variable::from(1), Sign::Positive)));
+        assert_eq!("Literal(-1)", format!("{:?}", Literal::from_var(Variable::from(1), Sign::Negative)));
+    }
+    #[test]
+    fn positive_builds_positive_lit(){
+        assert_eq!("Literal(1)", format!("{:?}", Literal::positive(Variable::from(1))));
+    }
+    #[test]
+    fn negative_builds_negative_lit(){
+        assert_eq!("Literal(-1)", format!("{:?}", Literal::negative(Variable::from(1))));
+    }
+    #[test]
+    fn var_returns_the_original_var(){
+        let v = Variable::from(42);
+
+        assert_eq!(v, Literal::positive(v).var());
+        assert_eq!(v, Literal::negative(v).var());
+    }
+    #[test]
+    fn sign_is_positive_for_positive_lits(){
+        let v = Variable::from(42);
+
+        if let Sign::Positive = Literal::positive(v).sign() {
+            assert!(true);
+        } else {
+            panic!("Should have been positive")
+        }
+    }
+    #[test]
+    fn sign_is_negative_for_negative_lits(){
+        let v = Variable::from(42);
+
+        if let Sign::Negative = Literal::negative(v).sign() {
+            assert!(true);
+        } else {
+            panic!("Should have been negative")
+        }
+    }
+    #[test]
+    fn to_isize_should_return_raw_value() {
+        assert_eq!(-42, Literal::from(-42).to_isize());
+    }
+
+    #[test]
+    fn test_equality() {
+        let a = Literal::from(-1);
+        let b = Literal::negative(Variable::from(1));
+        let c = Literal::from_var(Variable::from(1), Sign::Negative);
+
+        // reflexive
+        assert_eq!(a, a);
+        // transitive
+        assert_eq!(a, b);
+        assert_eq!(b, c);
+        assert_eq!(a, c);
+        // symmetric
+        assert_eq!(a, b);
+        assert_eq!(b, a);
+    }
+
+    #[test]
+    fn test_neg(){
+        let a = Literal::from(-1);
+        let b = Literal::positive(Variable::from(1));
+        let c = Literal::from_var(Variable::from(1), Sign::Positive);
+
+        assert_eq!(a, --a);
+        assert_eq!(a, -b);
+        assert_eq!(a, -c);
+    }
+    #[test]
+    fn test_not(){
+        let a = Literal::from(-1);
+        let b = Literal::positive(Variable::from(1));
+        let c = Literal::from_var(Variable::from(1), Sign::Positive);
+
+        assert_eq!(a, !!a);
+        assert_eq!(a, !b);
+        assert_eq!(a, !c);
+
+        assert_ne!(a, !a);
+        assert_ne!(a, b);
+        assert_ne!(a, c);
+
+        assert_ne!(a, Literal::from(32));
+        assert_ne!(a, Literal::from(-32));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use rsolve::branching::*;
 
 fn main() {
     let     now = SystemTime::now();
-    let    capa = 1_000;
+    let    capa = 100_000_000;
     let mut ord = VariableOrdering::new(capa);
 
     for i in 1..capa+1 { ord.bump(Variable::from(i), i); }
