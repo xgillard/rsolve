dba5c5c836ed86918811bfbc2656cf8acbb850ac
Xavier Gillard <xavier.gillard@uclouvain.be>
2017-11-09 14:13:42 +0100

    cosmetic heading/footage in main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use rsolve::*;
 // TODO: supporter plus de types d'input
 // TODO: etre plus bas niveau avec mes alias
 fn main() {
+    print_header();
     let stdin = stdin();
     let lock = stdin.lock();
     let mut lines = lock.lines();
@@ -19,13 +20,26 @@ fn main() {
 
     let satisfiable = solver.solve();
 
+    print_result(&solver, satisfiable);
+}
+
+fn print_header() {
+    println!("c *************************************************************************");
+    println!("c This is the `rsolve` SAT solver version 0.1.0");
+    println!("c -------------------------------------------------------------------------");
+    println!("c Copyright 2017 Xavier Gillard, Université Catholique de Louvain");
+    println!("c This software is licensed to you under the terms of the MIT license");
+    println!("c =========================================================================");
+}
+
+fn print_result(solver: &Solver, satisfiable: bool){
     if satisfiable {
         println!("s SATISFIABLE");
     } else {
         println!("s UNSATISFIABLE");
     }
-
+    println!("c -------------------------------------------------------------------------");
     println!("c nb_conflicts {}", solver.nb_conflicts);
     println!("c nb_restarts  {}", solver.nb_restarts);
-
+    println!("c *************************************************************************");
 }
