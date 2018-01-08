9b271c54cc8762aca84da7494f1e900a21bec8ff
Xavier Gillard <xavier.gillard@uclouvain.be>
2018-01-08 15:59:20 +0100

    aim at agile track

--- src/solving/solver.rs
+++ src/solving/solver.rs
@@ -108,7 +108,7 @@ impl Solver {
             var_order    : VariableOrdering::new(nb_vars as uint),
             phase_saving : Valuation::new(nb_vars),
             max_learned  : 1000,
-            restart_strat: LubyRestartStrategy::new(100),
+            restart_strat: LubyRestartStrategy::new(1),
             decisions    : vec![],
             decisions_pos: vec![],
             level        : VarIdxVec::from(vec![0; nb_vars]),
